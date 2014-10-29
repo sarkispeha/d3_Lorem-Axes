957af15a914458ecebb87b7e8c731a33b656ca0b
Simeg44 <Simeg44@gmail.com>
2014-10-29 17:16:21 -0600

    altered to work with new data structure

--- code-flower/javascripts/main.js
+++ code-flower/javascripts/main.js
@@ -1,6 +1,6 @@
 var myFlower = new CodeFlower("#flower", 1400, 800);
 var flowerData = data.children[0];
-flowerData.children = flowerData.children.slice(0,5);
+flowerData.children = flowerData.children.slice(0,1);
 myFlower.update(flowerData);
 
 
