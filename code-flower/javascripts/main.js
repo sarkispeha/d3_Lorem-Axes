410e86d1bdc55b8954fb4fbdf396b9d6e80bceb4
Simeg44 <Simeg44@gmail.com>
2014-10-30 14:13:27 -0600

    added lines and moved root

--- code-flower/javascripts/main.js
+++ code-flower/javascripts/main.js
@@ -1,6 +1,6 @@
 var myFlower = new CodeFlower("#flower", 1400, 800);
-var flowerData = data.children[0];
-flowerData.children = flowerData.children.slice(0,1);
+var flowerData = data.children[0].children[0];
+// flowerData.children = flowerData.children.slice(0,1);
 myFlower.update(flowerData);
 
 
