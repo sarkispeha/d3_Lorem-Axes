fc1abe3ed279074a803eb25c072c8c588df4960c
Kevin McCaw <kmmccaw@gmail.com>
2014-10-31 11:31:50 -0600

    Edited codeflower to have a larger bounding rectangle.

--- code-flower/javascripts/main.js
+++ code-flower/javascripts/main.js
@@ -1,5 +1,5 @@
 
-var myFlower = new CodeFlower("#flower", 1400, 800);
+var myFlower = new CodeFlower("#flower", 1400, 1200);
 var flowerData = data.children[0].children[0];
 // flowerData.children = flowerData.children.slice(0,1);
 myFlower.update(flowerData);
